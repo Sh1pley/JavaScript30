c9efd36d75a1c911b30c637c4f9b4c8daed34608
Jesse Shipley <jshipley@Jesses-MacBook-Pro-2.local>
2017-05-21 23:02:18 -0600

    finish functionality ...add tests?

--- 01 - JavaScript Drum Kit/drum-kit.js
+++ 01 - JavaScript Drum Kit/drum-kit.js
@@ -1,6 +1,28 @@
 
 
 window.addEventListener('keydown', (event) => {
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
-  console.log(audio)
-  })
+  playAudio();
+})
+
+window.addEventListener('keyup', (event) => {
+  fadeKey()
+})
+
+playAudio = () => {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+
+  if (!audio) return;
+  audio.currentTime = 0;
+  highlightKey();
+  audio.play();
+}
+
+highlightKey = () => {
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  if (key) key.classList.add('playing');
+}
+
+fadeKey = () => {
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  if (key) key.classList.remove('playing');
+}
